a164ea387ba392a7d6bd50c7ec549fd0529f977a
Josh Dawson <joshd@wson.me>
2024-04-11 11:40:06 +0100

    attempt initial photoswipe on single post pages

---
+++ static/photoswipe.js
@@ -0,0 +1,10 @@
+
+import PhotoSwipeLightbox from 'https://unpkg.com/photoswipe/dist/photoswipe-lightbox.esm.js';
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '#my-gallery',
+  children: 'img',
+  pswpModule: () => import('https://unpkg.com/photoswipe'),
+});
+
+lightbox.init();
