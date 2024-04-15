83b6dbfb6764ff3e184ca8ec2e7485c4f9a20f31
Josh Dawson <joshd@wson.me>
2024-04-15 18:24:04 +0100

    Another attempt at Masonry

--- static/masonry.js
+++ static/masonry.js
@@ -18,4 +18,8 @@
 
 imagesLoaded( document.querySelector('.grid'), function( instance ) {
     console.log('all images are loaded');
+    var msnry = new Masonry(grid, {
+                  itemSelector: '.grid-item',
+                  columnWidth: 200
+              });
   });
