3ea0c4fd3a5135c2e84f5569b3228e0fe3b96c9e
Josh Dawson <joshd@wson.me>
2024-04-15 19:24:22 +0100

    watch out for some weird behaviour

--- static/masonry.js
+++ static/masonry.js
@@ -1,7 +1,7 @@
 imagesLoaded( document.querySelector('.grid'), function( instance ) {
     console.log('all images are loaded');
-    document.getElementById("photo-spinner").classList.remove("d-flex");
-    document.getElementById("photo-spinner").classList.add("d-none");
+    // document.getElementById("photo-spinner").classList.remove("d-flex");
+    // document.getElementById("photo-spinner").classList.add("d-none");
     var photogrid = document.getElementById('photo-grid');
     photogrid.style.display = 'block';
     var msnry = new Masonry('.grid', {
