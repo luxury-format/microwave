835d57d356f71522eede503640521586201ea959
Josh Dawson <joshd@wson.me>
2024-04-15 18:59:09 +0100

    change the order of events

--- static/masonry.js
+++ static/masonry.js
@@ -1,10 +1,11 @@
 imagesLoaded( document.querySelector('.grid'), function( instance ) {
     console.log('all images are loaded');
-    var msnry = new Masonry('.grid', {
-                  itemSelector: '.grid-item',
-              });
     var spinner = document.getElementById('photo-spinner');
     spinner.style.display = 'none';
     var photogrid = document.getElementById('photo-grid');
     photogrid.style.display = 'block';
+    var msnry = new Masonry('.grid', {
+                  itemSelector: '.grid-item',
+              });
+    
   });
