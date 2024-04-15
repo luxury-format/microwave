21eb1092bd012204d139545a62cba335ffad469c
Josh Dawson <joshd@wson.me>
2024-04-15 18:38:37 +0100

    add a spinner to indicate photo loading

--- static/masonry.js
+++ static/masonry.js
@@ -1,21 +1,3 @@
-// Wait for images to load before initializing Masonry
-// document.addEventListener('DOMContentLoaded', function() {
-//     var grid = document.querySelector('.grid');
-
-//     console.log("I am alive")
-
-//     imagesLoaded(grid), function(instance) {
-//         console.log("images are loaded");
-//     }
-
-//     imagesLoaded(grid, function() {
-//         var msnry = new Masonry(grid, {
-//             itemSelector: '.grid-item',
-//             columnWidth: 200
-//         });
-//     });
-// });
-
 imagesLoaded( document.querySelector('.grid'), function( instance ) {
     console.log('all images are loaded');
     var msnry = new Masonry('.grid', {
