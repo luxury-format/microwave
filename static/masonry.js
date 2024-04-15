a418068339f12f27e5fecfa6359393bd80061f88
Josh Dawson <joshd@wson.me>
2024-04-15 19:20:01 +0100

    definitely get rid of the spinner

--- static/masonry.js
+++ static/masonry.js
@@ -1,7 +1,7 @@
 imagesLoaded( document.querySelector('.grid'), function( instance ) {
     console.log('all images are loaded');
     var spinner = document.getElementById('photo-spinner');
-    spinner.style.display = 'none';
+    spinner.style.display = 'none !important';
     var photogrid = document.getElementById('photo-grid');
     photogrid.style.display = 'block';
     var msnry = new Masonry('.grid', {
