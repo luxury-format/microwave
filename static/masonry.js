01872a6c2195ac210f05bb697e01520bb4daa400
Josh Dawson <joshd@wson.me>
2024-04-15 18:34:46 +0100

    remove fixed column width in masonry itself

--- static/masonry.js
+++ static/masonry.js
@@ -20,6 +20,5 @@ imagesLoaded( document.querySelector('.grid'), function( instance ) {
     console.log('all images are loaded');
     var msnry = new Masonry('.grid', {
                   itemSelector: '.grid-item',
-                  columnWidth: 200
               });
   });
