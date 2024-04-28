12df02ca73fc4d2f79549941bce23f1f34e33b3f
Josh Dawson <joshd@wson.me>
2024-04-28 10:41:00 +0100

    lastfm in sidemeta, archive off by default

--- static/js/lastfm.js
+++ static/js/lastfm.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     fetch('https://api.wson.me/api/lastfm/recent?limit=1', {
         headers: {
-            'X-API-KEY': 'ke3rwwTJgpvBqGeo6hOPltQ0Gz0KhrUwyvI0tSYFFXSAb3l1C1TtUME861mNcv5bVkLZyOAfcnNi0ITGrm043OVxCe3IEuw4RkmEEPpwOhwi1v3OiPfqDK7hu8GzWGRm' // Replace with your real key
+            'X-API-KEY': 'ke3rwwTJgpvBqGeo6hOPltQ0Gz0KhrUwyvI0tSYFFXSAb3l1C1TtUME861mNcv5bVkLZyOAfcnNi0ITGrm043OVxCe3IEuw4RkmEEPpwOhwi1v3OiPfqDK7hu8GzWGRm'
         }
     })
         .then(response => response.json())
@@ -13,11 +13,16 @@ document.addEventListener('DOMContentLoaded', function () {
             const largeImage = track.image.find(img => img.size === 'large')['#text'];
             
             document.getElementById('last-fm-art').src = largeImage;
+            document.getElementById('last-fm-art-side').src = largeImage;
             document.getElementById('last-fm-artist').textContent = artist;
+            document.getElementById('last-fm-artist-side').textContent = artist;
             document.getElementById('last-fm-title').textContent = title;
+            document.getElementById('last-fm-title-side').textContent = title;
             
             const spinnerOverlay = document.getElementById('spinner-overlay');
             spinnerOverlay.style.display = 'none';
+            const spinnerOverlaySide = document.getElementById('spinner-overlay-side');
+            spinnerOverlaySide.style.display = 'none';
         })
         .catch(error => console.error('Error:', error))
 });
