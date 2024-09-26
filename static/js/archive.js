9cb9a8a0d131633d29c47d1b1f3ce6c965b2246b
Josh Dawson <joshd@wson.me>
2024-09-26 18:08:13 +0100

    half a job

--- static/js/archive.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var hash = window.location.hash;
-    if (hash) {
-        var element = document.querySelector(hash);
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth' });
-        }
-    }
-});
